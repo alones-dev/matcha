c296fa96c4bf9383c4fb816fc6285afed7848163
Kylian <kyliandaumont78@gmail.com>
2025-03-28 10:34:07 +0100

    rm README.md

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ echo "🚀 Démarrage du frontend (Next.js)..."
 cd matcha && npx run dev &
 
 echo "🚀 Démarrage du backend (NestJS)..."
-cd backend && npm run start:dev &
+cd backend && npm run start &
 
 wait
